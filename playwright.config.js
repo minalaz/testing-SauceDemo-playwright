c75498541d72bda127f24c2bc438714650df3418
minalaz <mina.djogo@gmail.com>
2024-01-18 16:50:28 +0100

    adjusting config file

--- playwright.config.js
+++ playwright.config.js
@@ -3,23 +3,68 @@ const { devices } = require("@playwright/test");
 module.exports = {
   use: {
     baseURL: "https://www.saucedemo.com/",
-    viewport: { width: 1280, height: 720 },
+    video: process.env.CI ? "retain-on-failure" : "off",
   },
+  workers: process.env.CI ? 4 : undefined,
   projects: [
     {
-      name: "Chromium",
+      name: "Chromium - Laptop",
       use: {
         ...devices["Desktop Chrome"],
+        viewport: { width: 1280, height: 720 },
         headless: true,
       },
     },
     {
-      name: "Microsoft Edge",
+      name: "Chromium - Mobile",
+      use: {
+        ...devices["Pixel 5"],
+        headless: true,
+      },
+    },
+    {
+      name: "Chromium - Tablet",
+      use: {
+        ...devices["iPad Pro 11 landscape"],
+        headless: true,
+      },
+    },
+    {
+      name: "Chromium - Desktop",
+      use: {
+        ...devices["Desktop Chrome"],
+        viewport: { width: 1280, height: 1024 },
+        headless: true,
+      },
+    },
+
+    {
+      name: "Microsoft Edge - Desktop",
       use: {
         ...devices["Desktop Edge"],
+        viewport: { width: 1280, height: 1024 },
         channel: "msedge",
         headless: true,
       },
     },
+
+    {
+      name: "Firefox - Laptop",
+      use: {
+        ...devices["Desktop Firefox"],
+        viewport: { width: 1280, height: 720 },
+        headless: true,
+      },
+    },
+
+    {
+      name: "Firefox - Desktop",
+      use: {
+        ...devices["Desktop Firefox"],
+        viewport: { width: 1280, height: 1024 },
+        headless: true,
+      },
+    },
   ],
+  reporter: [["list"], ["html", { outputFolder: "test-results" }]],
 };
